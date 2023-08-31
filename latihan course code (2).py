104ee4eed615a86907352a685d327ef7ec86dde8
ainintan <143487657+ainintan@users.noreply.github.com>
2023-08-31 16:33:55 +0700

    Add files via upload

---
+++ latihan course code (2).py
@@ -0,0 +1,17 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+#course code 2
+m = ['Alice', 'ants','Bob', 'badgers', 'Carol', 'cats']
+m.sort()
+print (m)
+spam = ['a','z','A','Z']
+spam.sort(key=str.lower)
+print (spam)
+
+
