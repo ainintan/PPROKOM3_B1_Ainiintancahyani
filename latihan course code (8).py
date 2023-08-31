af32e1861d10f1db8a8abdb8f10723c3854eb427
ainintan <143487657+ainintan@users.noreply.github.com>
2023-08-31 16:35:33 +0700

    Add files via upload

---
+++ latihan course code (8).py
@@ -0,0 +1,23 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+print('Hello world!'.startswith('Hello'))
+print('Hello world!'.endswith('world!'))
+print('abc123'.startswith('abcdef'))
+print('abc123'.endswith('12'))
+print('Hello world!'.startswith('Hello world!'))
+print('Hello world!'.endswith('Hello world!'))
+
+print('Hello'.rjust(10))
+print('Hello'.rjust(20))
+print('Hello World'.rjust(20))
+print('Hello'.ljust(10))
+print('Hello'.rjust(20, '*'))
+print('Hello'.ljust(20, '-'))
+print('Hello'.center(20))
+print('Hello'.center(20, '=')) 
