6c8c34bd5c0b78c9dc47bd9506d99f111e1dd9a4
ainintan <143487657+ainintan@users.noreply.github.com>
2023-08-31 16:34:30 +0700

    Add files via upload

---
+++ latihan course code (4).py
@@ -0,0 +1,21 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+#course code 4
+str="Py'thon"
+str
+str2='Py"thon'
+str2
+"\"OK, \"sampai ketemu lagi."
+teks = ("Python adalah bahasa pemrograman yang powerfull.\n Terbukti Python bisa dijalankan di segala platform OS.\n jadi, saatnya kita menggunakan Pyhton sebagai bahasa pemrograman kita \n sehari -hari. Salam Python Dahsyat! ")
+print (teks)
+kata ="""Python Bahasa pemrograman yang powerfull.
+...Terbukti Python bisa dijalankan di segala platform OS.
+...Jadi saatnya kita menggunakan Python sebagai bahasa pemrograman 
+...Sehari-hari. Salam Python Dahsyat!"""
+print (kata)
